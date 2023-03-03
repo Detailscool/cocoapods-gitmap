ba135587de88ee7d8179d57b4eea4b3b4fdb29da
Detailscool <496389006@qq.com>
2023-03-03 17:53:53 +0800

    支持全局统一branch

--- lib/cocoapods-gitmap/helper/git_map.rb
+++ lib/cocoapods-gitmap/helper/git_map.rb
@@ -5,15 +5,30 @@ module Pod
     @@hasGitMap = true
     GitMapFile = '.GitMap.yml'
     @@repo = nil
+    @@default_branch = nil
 
-    def self.set_repo(repo)
+    def self.set_repo(repo, default_branch)
       @@repo = repo
+      @@default_branch = default_branch
     end
 
     def self.get_git_url(name)
       mapping = self.mapping
       name = name.include?('/') ? name.split('/').first : name
       return mapping && @@repo ? mapping[@@repo][name] : nil
+    end
+
+    def self.get_git_commit_from_default_branch(git_url)
+      return unless @@default_branch
+      command = ['ls-remote',
+                 '--',
+                 git_url,
+                 @@default_branch]
+      output = Downloader::Git.execute_command('git', command)
+
+      encoded_branch_name = @@default_branch.dup.force_encoding(Encoding::ASCII_8BIT)
+      match = %r{([a-z0-9]*)\trefs\/(heads|tags)\/#{Regexp.quote(encoded_branch_name)}}.match(output)
+      match[1] unless match.nil?
     end
 
     def self.mapping
--- lib/cocoapods-gitmap/native/podfile.rb
+++ lib/cocoapods-gitmap/native/podfile.rb
@@ -9,15 +9,21 @@ module Pod
           requirements.each do |requirement|
             git_url = requirement[:git]
             if git_url
-              requirement[:git] = GitMap.get_git_url(name) || git_url
+              new_git_url = GitMap.get_git_url(name)
+              requirement[:git] = new_git_url || git_url
+
+              commit = requirement[:commit]
+              if new_git_url && commit
+                requirement[:commit] = GitMap.get_git_commit_from_default_branch(new_git_url) || commit
+              end
             end
           end
         end
         old_pod(name, *requirements)
       end
 
-      def set_repo(repo)
-        GitMap.set_repo(repo)
+      def set_repo(repo, default_branch=nil)
+        GitMap.set_repo(repo, default_branch=default_branch)
       end
     end
   end
